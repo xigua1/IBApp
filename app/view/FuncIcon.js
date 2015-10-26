d3a16d63e3298f1a00e5a91b4633eaa782505899
Nan Li <job_linan@163.com>
2015-10-26 11:21:37 +0800

    + 增加FuncIcon类
    用于在首页，根据用户的权限 动态显示所需的功能模块

---
+++ app/view/FuncIcon.js
@@ -0,0 +1,32 @@
+Ext.define('IBApp.view.FuncIcon', {
+	extend: 'Ext.Panel',
+	requires: ['Ext.Img'],
+	xtype: 'funciconview',
+
+	config: {
+		flex: 1,
+		margin: 5,
+		imageId: '',
+		imageSrc: '',
+		text: '',
+	},
+
+	initialize: function () {
+
+	    this.callParent(arguments);
+
+	    var image = Ext.create('Ext.Img', {
+	    	itemId: this.config.imageId,
+	    	src: this.config.imageSrc,
+	    	style: 'width:100%;height:80%;margin:auto',
+	    });
+
+	    this.add([
+        	image,
+        ]);
+	    this.setHtml(['<p style="text-align:center">',
+	    	          this.config.text,
+	    	          '</p>'].join(""));
+	},
+
+});
