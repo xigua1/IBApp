5bc90f661293ecb96e18bcc2acf144cbb852b5b8
Nan Li <job_linan@163.com>
2015-10-27 17:26:53 +0800

    * 调整首页userProfile图片和文字的间距

--- app/view/UserInfoList.js
+++ app/view/UserInfoList.js
@@ -1,6 +1,6 @@
 var userInfoTemplate = new Ext.XTemplate(
 	'<tpl for=".">',
-	'<img width=100px height=100px style="float:left;margin-right:80px;margin-left:10px" src="{imgURL}" />',
+	'<img width=100px height=100px style="float:left;margin-right:40px;margin-left:10px" src="{imgURL}" />',
 	'<div class="list-item-title">{userName}<sup>2</sup></div>',
 	'<div class="list-item-narrative">',
 	'<p>{id}</p>',
