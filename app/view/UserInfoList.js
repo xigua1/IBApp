b7455f3f10da726a0d705a0b94d522b805082408
Nan Li <job_linan@163.com>
2015-10-29 16:52:57 +0800

    * 将userProfile的css class和普通列表的样式类区分开

--- app/view/UserInfoList.js
+++ app/view/UserInfoList.js
@@ -1,8 +1,8 @@
 var userInfoTemplate = new Ext.XTemplate(
 	'<tpl for=".">',
 	'<img width=100px height=100px style="float:left;margin-right:40px;margin-left:10px" src="{imgURL}" />',
-	'<div class="list-item-title">{userName}<sup>2</sup></div>',
-	'<div class="list-item-narrative">',
+	'<div class="user-profile-title">{userName}<sup>2</sup></div>',
+	'<div class="user-profile-narrative">',
 	'<p>{id}</p>',
 	'<p>{userRole}</p>',
 	'</div>',
