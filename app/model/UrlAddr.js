8a85fffb7483945a4d281275987d9102ebed22dc
tjwang0812 <tjwang0812@hotmail.com>
2015-11-25 14:14:13 +0800

    *删除UrlAddrmodel

--- app/model/UrlAddr.js
+++
@@ -1,9 +0,0 @@
-Ext.define("IBApp.model.UrlAddr", {
-    extend: "Ext.data.Model",
-    config: {
-        fields: [
-            { name: 'urlLogin', type: 'string' },
-            { name: 'urlServer', type: 'string' },
-        ]
-    }
-});
--- app/store/UrlAddr.js
+++ app/store/UrlAddr.js
@@ -3,8 +3,11 @@ Ext.define("IBApp.store.UrlAddr", {
     xtype: 'urladdrstore',
 
     config: {
-        model: "IBApp.model.UrlAddr",
-     data: [
+        fields: [
+            { name: 'urlLogin', type: 'string' },
+            { name: 'urlServer', type: 'string' },
+        ]
+        data: [
         {
             urlLogin: 'http://10.2.49.251:8080/pactera-jeesite/restService/userservice/0.2',
             urlServer: 'http://10.2.49.250:8080/mtservice/restService/0.1',
